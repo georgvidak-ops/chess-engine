3d71e3a1f63d5940da61dd9d1510ab98d30664b1
Giorgos Vidakis <georgvidak@gmail.com>
2026-04-27 20:53:46 +0300

    working movement (no validation or turn-taking)

--- board.py
+++ board.py
@@ -4,6 +4,23 @@ class Board:
     def __init__(self):
         self.board = self.create_starting_board()
 
+    numsArray = "abcdefgh"
+
+    def makeMove(self, r1, f1, r2, f2):
+        board = self.board
+
+        # file conversion: a-h → 0-7
+        f1 = ord(f1) - ord('a')
+        f2 = ord(f2) - ord('a')
+
+        # rank conversion: '1'-'8' → 7-0
+        r1 = 8 - int(r1)
+        r2 = 8 - int(r2)
+
+        board[r2][f2] = board[r1][f1]
+        board[r1][f1] = "."
+
+
     def create_starting_board(self):
         return [
             ["r","n","b","q","k","b","n","r"],
--- main.py
+++ main.py
@@ -11,8 +11,10 @@ def main():
         if move == "q":
             break
         
-        # for now, just print it (we’ll validate later)
-        print(f"You entered: {move}")
+        f_file, f_rank, t_file, t_rank = move
+
+        board.makeMove(f_rank, f_file, t_rank, t_file)
+
 
 if __name__ == "__main__":
     main()
