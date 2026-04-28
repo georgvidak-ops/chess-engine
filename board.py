54973195aa4c9ad15e65514e8bf8fa1956eb6612
Giorgos Vidakis <georgvidak@gmail.com>
2026-04-28 04:20:20 +0300

    Pawn Movement Validity (no en passant)

--- board.py
+++ board.py
@@ -1,10 +1,49 @@
-# board.py
-
 class Board:
     def __init__(self):
         self.board = self.create_starting_board()
 
-    numsArray = "abcdefgh"
+
+    def checkValidity(self, r1, f1, r2, f2):
+        board = self.board
+
+        # file conversion: a-h → 0-7
+        f1 = ord(f1) - ord('a')
+        f2 = ord(f2) - ord('a')
+
+        # rank conversion: '1'-'8' → 7-0
+        r1 = 8 - int(r1)
+        r2 = 8 - int(r2)
+
+        color = (board[r1][f1]).isupper() # white = true, black = false
+
+        if not (all(0 <= x <= 7 for x in (r1, f1, r2, f2))):
+            return False
+
+        if board[r1][f1] == "p" or board[r1][f1] == "P": # Pawn Check
+            return self.pawnValidity(r1, f1, r2, f2, color)
+
+
+    def pawnValidity(self, r1, f1, r2, f2, clr):
+        board = self.board
+        step = 1
+
+        if (clr and r1 == 6) or (not clr and r1 == 1): #check if pawn has moved to see if we can move it 2 squares or not
+            step = 2
+
+        target = board[r2][f2]
+
+        if (f1 == f2) and target == "." and abs(r2 - r1) <= step: #check if move is valid in terms of the piece being a pawn
+            if abs(r2 - r1) == 2:
+                if (clr and board[r1-1][f1] != ".") or (not clr and board[r1+1][f1] != "."): #check if we jumped over pieces when moving the pawn 2 squares
+                    return False
+            if (clr and (r2 - r1) < 0) or (not clr and (r2 - r1) > 0): #check if move is valid given the color
+                return True
+        elif abs(f1 - f2) == 1 and target != "." and target.isupper() != clr and abs(r2 - r1) == 1: #check if move is valid in terms of the piece being a pawn
+            if (clr and (r2 - r1) < 0) or (not clr and (r2 - r1) > 0): #check if move is valid given the colour
+                return True
+        #no en passant yet
+        return False
+
 
     def makeMove(self, r1, f1, r2, f2):
         board = self.board
--- main.py
+++ main.py
@@ -13,7 +13,12 @@ def main():
         
         f_file, f_rank, t_file, t_rank = move
 
-        board.makeMove(f_rank, f_file, t_rank, t_file)
+        if board.checkValidity(f_rank, f_file, t_rank, t_file):
+            print("Valid Move")
+            board.makeMove(f_rank, f_file, t_rank, t_file)
+        else:
+            print("Invalid Move")
+            break
 
 
 if __name__ == "__main__":
