5ef7b04ded1d4778a0aa7f7a578ce7465d975719
Giorgos Vidakis <georgvidak@gmail.com>
2026-05-04 17:48:53 +0300

    King cant castle if castling square is attacked by enemy piece

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def main():
         board.castling = False
         print("King is in check") if board.king_in_check(board.white_to_move) else print("King is Safe")
 
+        def avoid_check(sq_from, sq_to, clr):
+            if board.king_in_check(clr):
+                board.unmakeMove_sq(sq_from, sq_to, False)
+                print("Invalid move, king will be captured")
+                return True
+            return False
+
         move = input("Enter move (e.g. e2e4) (or enter q to quit the program): ")
 
         if move == "q":
@@ -18,6 +25,8 @@ def main():
                 if board.CheckCastling(True, board.white_to_move):
                     sq_from, sq_to = board.parse("e1g1") if board.white_to_move else board.parse("e8g8")
                     board.makeMove_sq(sq_from, sq_to, True)
+                    if avoid_check(sq_from, sq_to, board.white_to_move): #see if castling is possible (threats)
+                        continue
                     sq_from, sq_to = board.parse("h1f1") if board.white_to_move else board.parse("h8f8")
                     board.makeMove_sq(sq_from, sq_to, False)
                     continue
@@ -27,6 +36,8 @@ def main():
                 if board.CheckCastling(False, board.white_to_move):
                     sq_from, sq_to = board.parse("e1c1") if board.white_to_move else board.parse("e8c8")
                     board.makeMove_sq(sq_from, sq_to, True)
+                    if avoid_check(sq_from, sq_to, board.white_to_move): #check if castling is possible (threats)
+                        continue
                     sq_from, sq_to = board.parse("a1d1") if board.white_to_move else board.parse("a8d8")
                     board.makeMove_sq(sq_from, sq_to, False)
                     continue
@@ -40,9 +51,7 @@ def main():
 
         if board.checkValidity_sq(sq_from, sq_to):
             board.makeMove_sq(sq_from, sq_to, False)
-            if board.king_in_check(not board.white_to_move):
-                board.unmakeMove_sq(sq_from, sq_to, False)
-                print("Invalid move, king will be captured")
+            if avoid_check(sq_from, sq_to, not board.white_to_move):
                 continue
 
             print("Valid move")
